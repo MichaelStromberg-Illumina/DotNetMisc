8ff8e4e31bb3658605add178f58210c2466da90b
Michael Stromberg <mstromberg@illumina.com>
2020-04-10 20:11:35 -0700

    Added .NET Core 3.1 and single file assembly upgrade to BuildNirvana.sh

--- BuildNirvana.sh
+++ BuildNirvana.sh
@@ -6,8 +6,39 @@ then
 	exit 1
 fi
 
+# grab the major OS version
+MAJOR_VER="$(rpm --eval %{centos_ver})"
+
+# check if we have the CentOS 6 version of libBlockCompression.so
+echo '2ed28ea95d88c238df90c244b9409d92  /build/Compression/Packages/BlockCompression/libBlockCompression.so' | md5sum --status -c
+CENTOS6_LIB_STATUS=$?
+
+if [ $MAJOR_VER == "6" ] && [ $CENTOS6_LIB_STATUS -eq 1 ]
+then
+	echo "Detected CentOS6 and an invalid compression library: updating libBlockCompression.so"
+	cp /usr/local/lib/libBlockCompression.so /build/Compression/Packages/BlockCompression
+fi
+
+cd /build
+
+# check if we need to upgrade the projects
+if grep -q "netcoreapp2.1" Nirvana/Nirvana.csproj
+then
+	# update all projects to .NET core 3.1
+	echo "Updating all projects to .NET Core 3.1"
+	find . -name "*.csproj" -exec sed -i 's/netcoreapp2.1/netcoreapp3.1/g' {} \;
+	
+	# add single file assembly
+	echo "Adding single file assembly updates to csproj files"
+	SED_REPLACE='s/<\/PropertyGroup>/  <PublishTrimmed>true<\/PublishTrimmed>\n    <PublishReadyToRun>true<\/PublishReadyToRun>\n    <PublishSingleFile>true<\/PublishSingleFile>\n  <\/PropertyGroup>\n  <ItemGroup>\n    <RuntimeHostConfigurationOption Include="System.Globalization.Invariant" Value="true" \/>\n  <\/ItemGroup>/g'
+	sed -i "$SED_REPLACE" Nirvana/Nirvana.csproj
+	sed -i "$SED_REPLACE" Downloader/Downloader.csproj
+fi
+
+# build Nirvana
 cd /build/Nirvana
 dotnet publish -r rhel.6-x64 -c Release
 
+# build Downloader
 cd ../Downloader
 dotnet publish -r rhel.6-x64 -c Release
