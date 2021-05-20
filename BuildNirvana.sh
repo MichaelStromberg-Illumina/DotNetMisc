dfca03a2ff082b0ca5272e4bb7b04f3ede363aaa
Michael Stromberg <mstromberg@illumina.com>
2021-05-19 17:41:38 -0700

    Updated Native compilation to use .NET 5

--- BuildNirvana.sh
+++ BuildNirvana.sh
@@ -1,47 +1,29 @@
 #!/bin/sh
 
 if [ ! -f "/build/Nirvana.sln" ]
-then 
+then
 	echo -e "ERROR: Could not find /build/Nirvana.sln. Did you mount the volume in Docker?"
 	exit 1
-fi
-
-# grab the major OS version
-MAJOR_VER="$(rpm --eval %{centos_ver})"
-
-# check if we have the CentOS 6 version of libBlockCompression.so
-echo '2ed28ea95d88c238df90c244b9409d92  /build/Compression/Packages/BlockCompression/libBlockCompression.so' | md5sum --status -c
-CENTOS6_LIB_STATUS=$?
-
-if [ $MAJOR_VER == "6" ] && [ $CENTOS6_LIB_STATUS -eq 1 ]
-then
-	echo "Detected CentOS6 and an invalid compression library: updating libBlockCompression.so"
-	cp /usr/local/lib/libBlockCompression.so /build/Compression/Packages/BlockCompression
 fi
 
 cd /build
 
 # check if we need to upgrade the projects
-if grep -q "netcoreapp2.1" Nirvana/Nirvana.csproj
+if grep -q "netcoreapp3.1" Nirvana/Nirvana.csproj
 then
-	# update all projects to .NET core 3.1
-	echo "Updating all projects to .NET Core 3.1"
-	find . -name "*.csproj" -exec sed -i 's/netcoreapp2.1/netcoreapp3.1/g' {} \;
-	
+	# update all projects to .NET 5.0
+	echo "Updating all projects to .NET 5.0"
+	find . -name "*.csproj" -exec sed -i 's/netcoreapp3.1/net5.0/g' {} \;
+
 	# add single file assembly
 	echo "Adding single file assembly updates to csproj files"
-	SED_REPLACE='s/<\/PropertyGroup>/  <PublishTrimmed>true<\/PublishTrimmed>\n    <PublishReadyToRun>true<\/PublishReadyToRun>\n    <PublishSingleFile>true<\/PublishSingleFile>\n  <\/PropertyGroup>\n  <ItemGroup>\n    <RuntimeHostConfigurationOption Include="System.Globalization.Invariant" Value="true" \/>\n  <\/ItemGroup>/g'
+	SED_REPLACE='s/<\/PropertyGroup>/  <PublishTrimmed>true<\/PublishTrimmed>\n    <IncludeAllContentForSelfExtract>true<\/IncludeAllContentForSelfExtract>\n    <PublishReadyToRun>true<\/PublishReadyToRun>\n    <PublishSingleFile>true<\/PublishSingleFile>\n  <\/PropertyGroup>\n  <ItemGroup>\n    <RuntimeHostConfigurationOption Include="System.Globalization.Invariant" Value="true" \/>\n  <\/ItemGroup>/g'
 	sed -i "$SED_REPLACE" Nirvana/Nirvana.csproj
 	sed -i "$SED_REPLACE" Downloader/Downloader.csproj
 fi
 
 # set the appropriate runtime ID
 RUNTIME_ID=linux-x64
-
-if [ $MAJOR_VER == "6" ]
-then
-	RUNTIME_ID=rhel.6-x64
-fi
 
 # get the git version
 NIRVANA_VER="$(git describe --long | cut -c2-)"
